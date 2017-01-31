d10c310a09d2c13bbbb8a9e6c8160118dfc1a347
mike <mike@zeoalliance.com>
2017-01-31 18:15:08 +0200

    update exchange package, add .gitignore

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -1,12 +1,16 @@
 package exchange
 
-import "net/http"
+import (
+	"net/http"
+	"encoding/json"
+	"errors"
+)
 
 const url string = "https://obmenka.kharkov.ua/api/rates/020016"
 
 type LatestRates struct {
-	WholeSale	float32
-	WholeBuy	float32
+	WholeSale	float32		`json:"wholeSale"`
+	WholeBuy	float32		`json:"wholeBuy"`
 }
 
 type BankUnitRates struct {
@@ -23,14 +27,24 @@ type Response struct {
 	ID		int
 	From		string
 	To		string
-	LatestRates	LatestRates
-	BankRates	BankRates
+	LatestRates	LatestRates	`json:"latestRates"`
+	BankRates	BankRates	`json:"bankRates"`
 }
 
-func GetUSD() (*string, error) {
+func GetUSD() (*Response, error) {
 	rsp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
+	resps := make([]Response, 0)
 
+	err = json.NewDecoder(rsp.Body).Decode(&resps)
+	if err != nil {
+		return nil, err
+	}
+	if len(resps) > 0 {
+		return &resps[0], nil
+	}
+
+	return nil, errors.New("Empty response")
 }
--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	ui "github.com/gizak/termui"
 	"time"
+	"github.com/bolsunovskyi/infolcd/exchange"
+	"fmt"
 )
 
 func main() {
+
+
+
 	err := ui.Init()
 	if err != nil {
 		panic(err)
@@ -16,9 +21,26 @@ func main() {
 	timePar.Height = 3
 	timePar.BorderLabel = "Date/Time"
 	timePar.Width = 5
-	col := ui.NewCol(4, 4, timePar)
 
-	ui.Body.AddRows(ui.NewRow(col));
+	usd, err := exchange.GetUSD()
+	var usdVal string
+	if err != nil {
+		usdVal = err.Error()
+	} else {
+		usdVal = fmt.Sprintf("%.2f/%.2f", usd.LatestRates.WholeBuy, usd.LatestRates.WholeSale)
+	}
+
+	excPar := ui.NewPar(usdVal)
+	excPar.Height = 3
+	excPar.BorderLabel = "Exchange Rate"
+	excPar.Width = 5
+
+
+	excCol := ui.NewCol(4, 0, excPar)
+	timeCol := ui.NewCol(4, 0, timePar)
+
+
+	ui.Body.AddRows(ui.NewRow(timeCol, excCol));
 	ui.Body.Align()
 	ui.Render(ui.Body)
 
@@ -38,6 +60,7 @@ func main() {
 		timePar.Text = time.Now().Format("02.01.2006 15:04:05");
 		ui.Render(ui.Body)
 	})
+
 
 
 	ui.Loop()
