4d206a8a9dc45f04f01288d7f9cac9d8aba072f2
mike <mike@zeoalliance.com>
2017-01-31 18:43:59 +0200

    add weather

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 	"github.com/bolsunovskyi/infolcd/exchange"
 	"fmt"
+	"github.com/bolsunovskyi/infolcd/weather"
 )
 
 func main() {
@@ -17,8 +18,8 @@ func main() {
 	}
 	defer ui.Close()
 
-	timePar := ui.NewPar(time.Now().Format("02.01.2006 15:04:05"))
-	timePar.Height = 3
+	timePar := ui.NewPar(time.Now().Format("02.01.2006\n15:04:05"))
+	timePar.Height = 4
 	timePar.BorderLabel = "Date/Time"
 	timePar.Width = 5
 
@@ -31,16 +32,30 @@ func main() {
 	}
 
 	excPar := ui.NewPar(usdVal)
-	excPar.Height = 3
+	excPar.Height = 4
 	excPar.BorderLabel = "Exchange Rate"
 	excPar.Width = 5
+
+	w, err := weather.GetWeather()
+	var weatherVal string
+	if err != nil {
+		weatherVal = err.Error()
+	} else {
+		weatherVal = fmt.Sprintf("%.1f, %s\n%s", w.Main.Temp, w.Weather[0].Main, w.Weather[0].Description)
+	}
+
+	weatherPar := ui.NewPar(weatherVal)
+	weatherPar.Height = 4
+	weatherPar.BorderLabel = "Weather"
+	weatherPar.Width = 5
 
 
 	excCol := ui.NewCol(4, 0, excPar)
 	timeCol := ui.NewCol(4, 0, timePar)
+	weatherCol := ui.NewCol(4, 0, weatherPar)
 
 
-	ui.Body.AddRows(ui.NewRow(timeCol, excCol));
+	ui.Body.AddRows(ui.NewRow(timeCol, excCol, weatherCol));
 	ui.Body.Align()
 	ui.Render(ui.Body)
 
@@ -57,7 +72,7 @@ func main() {
 	})
 
 	ui.Handle("/timer/1s", func(e ui.Event) {
-		timePar.Text = time.Now().Format("02.01.2006 15:04:05");
+		timePar.Text = time.Now().Format("02.01.2006\n15:04:05");
 		ui.Render(ui.Body)
 	})
 
---
+++ weather/weather.go
@@ -0,0 +1,41 @@
+package weather
+
+import (
+	"encoding/json"
+	"net/http"
+	"fmt"
+)
+
+const key string = "26f430f11e19862ae62c3b11fbea4c37"
+const city string = "703448"
+const url string = "http://api.openweathermap.org/data/2.5/weather?id=%s&apikey=%s&units=metric"
+
+type Weather struct {
+	Main		string
+	Description	string
+}
+
+type Main struct {
+	Temp		float32
+	Pressure	float32
+	Humidity	float32
+}
+
+type Response struct {
+	Weather		[]Weather
+	Main		Main
+}
+
+func GetWeather() (*Response, error) {
+	rsp, err := http.Get(fmt.Sprintf(url, city, key))
+	if err != nil {
+		return nil, err
+	}
+	resp := Response{}
+
+	err = json.NewDecoder(rsp.Body).Decode(&resp)
+	if err != nil {
+		return nil, err
+	}
+	return &resp, nil
+}
